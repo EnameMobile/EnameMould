8a41b847292f28f387dbe5b0114210760673ab1f
叶翔 <yx30@outlook.com>
2015-10-27 15:07:10 +0800

    test

---
+++ app/src/main/java/com/ename/www/mould/ui/activity/MainActivity.java
@@ -0,0 +1,15 @@
+package com.ename.www.mould.ui.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.ename.www.mould.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
